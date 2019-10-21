3af208621d1ac12aa85903c9cb262bd66c6513fe
madonna reda <50421738+madonnasheko@users.noreply.github.com>
2019-10-21 21:55:13 +0200

    frist commit

---
+++ src/error_message.java
@@ -0,0 +1,28 @@
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Akram
+ */
+public class error_message extends JFrame{
+   
+    
+    error_message(String title ){
+        super(title);
+      
+      
+        
+        
+        
+    }
+    
+    
+}
---
+++ src/playfair/Playfair.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playfair;
+
+/**
+ *
+ * @author Akram
+ */
+public class Playfair {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
